41cce06a47bca237f0bbbe0459f488a6b12580b6
nayak <khageswarnayak2000@gmail.com>
2024-05-05 17:26:28 +0530

    Dto class for movei app

---
+++ src/main/java/com/babu24/moviebookingapplication/dto/MoveiDto.java
@@ -0,0 +1,38 @@
+package com.babu24.moviebookingapplication.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MoveiDto {
+
+    private Integer movieId;
+
+
+    @NotBlank(message = "please provide movie title")
+    private String title;
+
+    @NotBlank(message = "please provide movie director name")
+    private  String director;
+
+    @NotBlank(message = "please provide movie studio name")
+    private String studio;
+
+    private Set<String> movieCast;
+    private Integer releaseYear;
+
+    @NotBlank(message = "please provide movie poster photo")
+    private String poster;
+
+    @NotBlank(message = "please provide poster's url")
+    private String posterUrl;
+
+}
