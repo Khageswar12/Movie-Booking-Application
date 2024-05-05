11d03fcf2ade9854e7d9e08db5e507a81018b777
nayak <khageswarnayak2000@gmail.com>
2024-05-05 17:27:58 +0530

    update

--- src/main/java/com/babu24/moviebookingapplication/Entity/Movei.java
+++ src/main/java/com/babu24/moviebookingapplication/Entity/Movei.java
@@ -1,6 +1,7 @@
 package com.babu24.moviebookingapplication.Entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -21,15 +22,15 @@ public class Movei {
     private Integer movieId;
 
     @Column(nullable = false)
-    @NotNull(message = "please provide movie title")
+    @NotBlank(message = "please provide movie title")
     private String title;
 
     @Column(nullable = false)
-    @NotNull(message = "please provide movie director name")
+    @NotBlank(message = "please provide movie director name")
     private  String director;
 
     @Column(nullable = false)
-    @NotNull(message = "please provide movie studio name")
+    @NotBlank(message = "please provide movie's studio ")
     private String studio;
 
     @ElementCollection
@@ -37,11 +38,10 @@ public class Movei {
     private Set<String> movieCast;
 
     @Column(nullable = false)
-    @NotNull(message = "please provide movie release year name")
     private Integer releaseYear;
 
     @Column(nullable = false)
-    @NotNull(message = "please provide movie poster photo")
+    @NotBlank(message = "please provide movie poster photo")
     private String poster;
 
 }
